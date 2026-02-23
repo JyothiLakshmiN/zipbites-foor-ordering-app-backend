8cf40bd234b719d3232c10df875a5114207b0a8b
Jyothi Lakshmi N <jyothilakshmi.cbp@gmail.com>
2026-02-23 09:58:13 -0600

    adding view order features

--- src/routes/MyRestaurantRoutes.ts
+++ src/routes/MyRestaurantRoutes.ts
@@ -11,6 +11,18 @@ const upload = multer({
     storage: storage,
     limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
 });
+
+router.get('/order',
+    jwtCheck, 
+    jwtParse,
+    MyRestaurantController.getMyRestaurantOrder
+)
+
+router.patch('/order/:orderId/status',
+    jwtCheck, 
+    jwtParse,
+    MyRestaurantController.updateOrderStatus
+)
 router.post('/',
     upload.single("imageFile"), 
     jwtCheck, 
